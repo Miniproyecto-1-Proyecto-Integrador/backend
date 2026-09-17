4d3d7f7e37ed7189d4ddaed2f6d193227bd1c84b
manumorita <manuela.moncada@correounivalle.edu.co>
2026-09-17 01:24:18 -0500

    proteger SECRET_KEY con variable de entorno

--- config/settings.py
+++ config/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-turt(b#i^t0(vs68&qr((j2+^#&nc_jqyzurde2=g*)_^dsrr4'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
