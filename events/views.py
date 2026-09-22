ecfb6b21e8a54203a48d4085c00794a7389776e5
TofiTurtle <juan.rodriguez.lozano@correounivalle.edu.co>
2026-09-22 02:13:13 -0500

    feat: se implementa la US-3, ahora se pueden modificar y eliminar eventos y subtareas.De momento usando el Django rest Framework funciona PERFECTO nativamente desde el back

--- events/views.py
+++ events/views.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import NotFound
 from .models import Event,Subtask
 from .serializers import EventSerializer
 from .serializers import SubtaskSerializer
+from django.shortcuts import get_object_or_404
 
 ##view generica de health check para el back.
 def health_check(request):
@@ -12,13 +13,13 @@ def health_check(request):
         'message': 'Backend funcionando correctamente'
     })
 
-##Implementacion de view de eventos
+##Implementacion de view de eventos US-1
 class EventListCreateView(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
 
-##Implementacion de view de subtasks
+##Implementacion de view de subtasks US 2
 class SubtaskListCreateView(generics.ListCreateAPIView):
     serializer_class = SubtaskSerializer
 
@@ -33,3 +34,25 @@ class SubtaskListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(evento=self.get_event())
+
+
+##Implementacion de US-3
+##aqui ya tenemos creados los eventos, las subtasks, por lo que 
+##solo necesitamos que el back soporte otro tipo de peticiones
+class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """GET/PATCH/PUT/DELETE /api/events/<pk>/"""
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class SubtaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """GET/PATCH/PUT/DELETE /api/events/<event_id>/subtasks/<pk>/"""
+    serializer_class = SubtaskSerializer
+
+    def get_queryset(self):
+        # asegura que la subtarea pertenezca al evento de la URL
+        return Subtask.objects.filter(evento_id=self.kwargs['event_id'])
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.kwargs['pk'])
